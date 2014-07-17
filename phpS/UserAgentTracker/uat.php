09282f2c1c927cb293455acc2845cd082f2e15ae
gustavoem <estrela.gustavo.matos@gmail.com>
2014-07-17 13:16:10 -0300

    nao lembro rsrs

--- phpS/UserAgentTracker/uat.php
+++ phpS/UserAgentTracker/uat.php
@@ -1,3 +1,10 @@
-<?php
-echo $_SERVER['HTTP_USER_AGENT'];
-?>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>    
+	<?php
+		echo $_SERVER['HTTP_USER_AGENT'];
+	?>
+    </body>    
+</html>

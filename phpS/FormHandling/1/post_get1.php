94f54e23a98968939a61376d782c13d0778dfda6
gustavoem <estrela.gustavo.matos@gmail.com>
2014-07-22 21:57:47 -0300

    getpost

---
+++ phpS/FormHandling/1/post_get1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" 
+"http://www.w3.org/TR/html4/strict.dtd"> 
+<html> 
+<head> 
+    <title>PostGet1</title>        
+</head> 
+
+<body> 
+    <h2> Post form</h2> 
+    <form method="post" action="post_get2.php"> 
+    text:<input type="text" size =40 name="name"> 
+    <input type=submit value="Submit Post"> 
+    </form> 
+    
+    <h2> GET form</h2> 
+    <form method="get" action="post_get2.php"> 
+    text:<input type="text" size =40 name="name"> 
+    <input type=submit value="Submit Get" > 
+    </form> 
+
+</body> 
+</html>
